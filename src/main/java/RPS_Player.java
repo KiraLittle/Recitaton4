85f9e7a0682b1b42d002ce99f43f784cbf251047
alittle23 <akairalittle@gmail.com>
2024-10-16 16:46:35 -0400

    finished

--- src/main/java/RPS_Player.java
+++ src/main/java/RPS_Player.java
@@ -4,14 +4,19 @@ public class RPS_Player {
     private int numberOfGamesWon;
     private int numberOfGamesPlayed;
     private int choice;
+    private int aPlayer;
     private String name;
+    private Random rand = new Random();
 
     public RPS_Player(String name){
+        this.name = name;
         // TODO: replace this line with your code.
+
     }
 
     public String getName(){
         // TODO: replace this line with your code.
+        return name;
     }
 
     /**
@@ -20,6 +25,7 @@ public class RPS_Player {
      */
     public int getNumberOfGamesPlayed(){
         // TODO: replace this line with your code.
+        return numberOfGamesPlayed;
     }
 
     /**
@@ -28,6 +34,7 @@ public class RPS_Player {
      */
     public int getNumberOfGamesWon(){
         // TODO: replace this line with your code.
+        return numberOfGamesWon;
     }
 
     /**
@@ -36,6 +43,7 @@ public class RPS_Player {
      */
     public double getWinPercentage(){
         // TODO: replace this line with your code.
+        return ((double)getNumberOfGamesWon()/getNumberOfGamesPlayed());
     }
 
     /**
@@ -43,6 +51,8 @@ public class RPS_Player {
      */
     public void clear(){
         // TODO: replace this line with your code.
+        numberOfGamesWon = 0;
+        numberOfGamesPlayed = 0;
     }
 
     /**
@@ -52,9 +62,51 @@ public class RPS_Player {
      * @param anotherPlayer an RPS_Player that this player is challenging.
      * @return Reference to the RPS_Player that won or a null if there is a draw
      */
-    public RPS_Player challenge(RPS_Player anotherPlayer){
-        // TODO: replace this line with your code.
+    public RPS_Player challenge(RPS_Player anotherPlayer) {
+
+        // 1- ROCK  2- PAPER   3- SCISSORS
+
+        //player 1
+
+        choice = rand.nextInt(3) + 1;
+
+        //player 2
+
+        aPlayer = rand.nextInt(3) + 1;
+
+
+        if ((((choice == 1) && (aPlayer == 3)) || ((choice == 2) && (aPlayer == 1))) || ((choice == 3) && (aPlayer == 2))) {
+            System.out.println("Player 1 Wins!");
+            numberOfGamesWon += 1;
+            numberOfGamesPlayed += 1;
+            anotherPlayer.numberOfGamesPlayed +=1;
+            return this;
+        } else if ((((aPlayer == 1) && (choice == 3)) || ((aPlayer == 2) && (choice == 1))) || ((aPlayer == 3) && (choice == 2))) {
+            System.out.println("Player 2 Wins!");
+            numberOfGamesPlayed += 1;
+            anotherPlayer.numberOfGamesWon+=1;
+            anotherPlayer.numberOfGamesPlayed +=1;
+            return anotherPlayer;
+        } else if (aPlayer == choice) {
+            numberOfGamesPlayed += 1;
+            anotherPlayer.numberOfGamesPlayed +=1;
+            return null;
+        }
+
+        if (anotherPlayer.numberOfGamesPlayed<numberOfGamesWon ) {
+            System.out.println("Player 1 Wins!");
+            return this;
+            } else if(anotherPlayer.numberOfGamesWon== numberOfGamesWon) {
+                System.out.println("Tie!!");
+                return null;
+            }else{
+            System.out.println("Player 2 Wins!");
+            return anotherPlayer;
+
+        }
+
     }
+
 
     /**
      * This player challenges anotherPlayer whereby this player uses the previous
@@ -63,8 +115,48 @@ public class RPS_Player {
      * @param anotherPlayer an RPS_Player that this player is challenging.
      * @return Reference to the RPS_Player that won or a null if there is a draw
      */
+
     public RPS_Player keepAndChallenge(RPS_Player anotherPlayer){
-        // TODO: replace this line with your code.
+        // TODO: same as challenge but no op
+
+        // 1- ROCK  2- PAPER   3- SCISSORS
+
+        //player 2
+        aPlayer = rand.nextInt(3)+1;
+
+        //number of games player 2 won
+        if ((((choice == 1) && (aPlayer == 3)) || ((choice == 2) && (aPlayer == 1))) || ((choice == 3) && (aPlayer == 2))) {
+            System.out.println("Player 1 Wins!");
+            numberOfGamesWon += 1;
+            numberOfGamesPlayed += 1;
+            anotherPlayer.numberOfGamesPlayed +=1;
+            return this;
+        } else if ((((aPlayer == 1) && (choice == 3)) || ((aPlayer == 2) && (choice == 1))) || ((aPlayer == 3) && (choice == 2))) {
+            System.out.println("Player 2 Wins!");
+            numberOfGamesPlayed += 1;
+            anotherPlayer.numberOfGamesWon+=1;
+            anotherPlayer.numberOfGamesPlayed +=1;
+            return anotherPlayer;
+        } else if (aPlayer == choice) {
+            numberOfGamesPlayed += 1;
+            anotherPlayer.numberOfGamesPlayed +=1;
+            return null;
+        }
+
+        if (anotherPlayer.numberOfGamesPlayed<numberOfGamesWon ) {
+            System.out.println("Player 1 Wins!");
+            return this;
+        } else if(anotherPlayer.numberOfGamesWon== numberOfGamesWon) {
+            System.out.println("Tie!!");
+            return null;
+        }else{
+            System.out.println("Player 2 Wins!");
+            return anotherPlayer;
+
+
+        }
+
     }
+
 
 }
